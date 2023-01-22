6a708f1198130fdfe54d1d644fa2e7e343775727
Julian Piasecki <julian.piasecki@outlook.com>
2023-01-22 17:36:46 +0100

    Fixing

--- index.js
+++ index.js
@@ -3,27 +3,40 @@ const submitBtn = document.getElementById('submit');
 const dec = document.getElementById('decimal');
 
 const bin2dec = () => {
-  if (bin.value.includes(0) || bin.value.includes(1)) {
+  const binValue = bin.value;
+
+  if (
+    binValue.length >= 4 &&
+    Number(binValue) % 1 === 0 &&
+    !binValue.includes('2') &&
+    !binValue.includes('3') &&
+    !binValue.includes('4') &&
+    !binValue.includes('5') &&
+    !binValue.includes('6') &&
+    !binValue.includes('7') &&
+    !binValue.includes('8') &&
+    !binValue.includes('9')
+  ) {
     let decimals;
-    let binArray = bin.value.split('');
+    let binArray = binValue.split('');
 
     binArray = binArray.map((bin) => parseInt(bin));
 
     let binArrayIndex = -1;
     let bin2decPower = binArray.length;
 
-    for (const bin of binArray) {
+    for (const binNumber of binArray) {
       binArrayIndex += 1;
       bin2decPower -= 1;
 
-      binArray[binArrayIndex] = bin * 2 ** bin2decPower;
+      binArray[binArrayIndex] = binNumber * 2 ** bin2decPower;
     }
 
     decimals = binArray.reduce((a, b) => a + b);
 
     dec.innerText = `Decimal Number: ${decimals}`;
   } else {
-    dec.innerText = 'You entered number other than 0 and 1';
+    dec.innerText = '⛔ Error!';
   }
 };
 
